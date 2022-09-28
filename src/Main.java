1a07855ba09beb4fd1df6fcbfa6ca7856e845984
Alexandr <kitalex777@mail.ru>
2022-09-28 20:23:15 +0300

    commit 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,35 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Техническое задание
+        int clientOS = 0; // 0-iOS, 1-Android
+        int yearOfIssue = 0; // 0 - c 2015г и по настоящее время, 1 - произведен ранее 2015 года
+
+        // Задание 1.
+        System.out.println("Задание 1.");
+        System.out.println();
+
+        if (clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        }
+        if (clientOS == 0) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+        System.out.println();
+
+        // Задание 2.
+        System.out.println("Задание 2.");
+        if (clientOS == 0 && yearOfIssue == 1){
+            System.out.println("Установите приложения для iOS по ссылке");
+        } else if (clientOS == 0 && yearOfIssue == 0) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        }else if (clientOS == 1 && yearOfIssue == 0) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        }
+        else {
+            System.out.println("Установите приложения для Android по ссылке");
+        }
+
+
     }
+
 }
